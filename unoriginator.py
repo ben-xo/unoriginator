4ea2854949b7e255fa863fe7337f2806ba6fb930
Ben Isaacs <ben@last.fm>
2020-07-26 13:16:02 +0100

    Basic framework for reading the title tags from the files

--- unoriginator.py
+++ unoriginator.py
@@ -4,6 +4,12 @@ import xml.sax
 import re
 import urllib.parse
 
+from mutagen import MutagenError
+from mutagen.aiff import AIFF
+from mutagen.mp3 import MP3
+from mutagen.wave import WAVE
+
+
 class iTunesHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.depth = 0
@@ -27,9 +33,11 @@ class iTunesHandler(xml.sax.ContentHandler):
         self.currentTag = ""
         if self.inTrack:
             if tag == 'dict':
-                if re.match(r'^.*\(original mix\)$', self.trackName, flags=re.IGNORECASE):
+                if re.match(r'^.*\(original mix\)$', self.trackName, flags=re.I):
                     if self.trackLocation[0:8] == 'file:///':
-                        print(urllib.parse.unquote(self.trackLocation)[7:])
+                        filename = urllib.parse.unquote(self.trackLocation)[7:]
+                        print ("** %s" % filename)
+                        updateTag(filename)
                 self.trackName = ""
                 self.trackLocation = ""
                 self.inTrack = False
@@ -51,6 +59,24 @@ class iTunesHandler(xml.sax.ContentHandler):
                 elif self.nextStringIsLocation:
                     self.trackLocation += content
 
+
+def updateTag(filename):
+    try:
+        if re.match(r'^.*\.mp3', filename, re.I):
+            mp3 = MP3(filename)
+            print(mp3.tags.getall('TIT2'))
+        elif re.match(r'^.*\.aiff?', filename, re.I):
+            aiff = AIFF(filename)
+            print(aiff.tags.getall('TIT2'))
+        elif re.match(r'^.*\.wav', filename, re.I):
+            wav = WAVE(filename)
+            print(wav.tags.getall('TIT2'))
+        else:
+            print("!! Unsure what format %s is" % filename)
+    except MutagenError:
+        print("!! Could not read file")
+    except AttributeError:
+        print('!! No tags found')
 
 
 if (__name__ == "__main__"):
