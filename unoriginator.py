a5cc9cf4413ab6128e3f7138f5cc5eedfd50869a
Ben Isaacs <ben@last.fm>
2020-07-26 18:35:31 +0100

    Actually fix files, and print stats

--- unoriginator.py
+++ unoriginator.py
@@ -7,9 +7,14 @@ from pprint import pprint
 
 import mutagen
 from mutagen import MutagenError
-from mutagen.id3 import ID3
+from mutagen.id3 import ID3, TIT2
 
 ENDS_WITH_ORIGINAL_MIX = re.compile(r'^(.*?)\s*\(original mix\)$', re.I)
+
+total_seen_files = 0
+total_updated_files = 0
+total_itunes_entries = 0
+didnt_process = []
 
 class iTunesHandler(xml.sax.ContentHandler):
     def __init__(self):
@@ -30,6 +35,7 @@ class iTunesHandler(xml.sax.ContentHandler):
 
     # Call when an elements ends
     def endElement(self, tag):
+        global total_itunes_entries
         self.depth -= 1
         self.currentTag = ""
         if self.inTrack:
@@ -37,10 +43,11 @@ class iTunesHandler(xml.sax.ContentHandler):
                 if ENDS_WITH_ORIGINAL_MIX.match(self.trackName):
                     if self.trackLocation[0:8] == 'file:///':
                         filename = urllib.parse.unquote(self.trackLocation)[7:]
-                        updateTag(filename)
+                        update_tag(filename)
                 self.trackName = ""
                 self.trackLocation = ""
                 self.inTrack = False
+                total_itunes_entries += 1
             elif tag == 'string':
                 self.nextStringIsName = False
                 self.nextStringIsLocation = False
@@ -60,31 +67,50 @@ class iTunesHandler(xml.sax.ContentHandler):
                     self.trackLocation += content
 
 
-def updateTag(filename):
+def update_tag(filename):
+    global total_updated_files
+    global total_seen_files
+    global didnt_process
+
+    total_seen_files += 1
     try:
         print("** Checking %s" % filename)
+
+        # id3 = ID3(filename)
+        # print("++ ID3 version: {}".format(id3.version))
+
         f = mutagen.File(filename)
         metadata_title = f.get('TIT2')
 
         if metadata_title is None:
             print(".. No TIT2 in file.")
+            didnt_process.append(filename)
             return
 
         print("++ Found %s" % metadata_title.text[0])
         m = ENDS_WITH_ORIGINAL_MIX.match(metadata_title.text[0])
-        fixed_metadata_title = m[1]
-        if metadata_title == fixed_metadata_title:
-            print(".. Metadata title '%s' already looks fine to me." % metadata_title)
+        if m is not None:
+            fixed_metadata_title = m[1]
+            if metadata_title == fixed_metadata_title:
+                print(".. Metadata title '%s' already looks fine to me." % metadata_title)
+                return
+
+            f.tags.add(TIT2(text=[fixed_metadata_title]))
+            f.save()
+            total_updated_files += 1
             return
-        id3 = ID3(filename)
-        print("++ ID3 version: {}".format(id3.version))
-    except MutagenError:
-        print("!! Could not read file")
+        else:
+            print(".. Did mot match (original mix) for {}".format(filename))
+
+    except MutagenError as e:
+        print("!! Could not read file: {}".format(e))
     except AttributeError:
-        print('!! No tags found')
+        print('!! No tags found: {}'.format(e))
+
+    didnt_process.append(filename)
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     # create an XMLReader
     parser = xml.sax.make_parser()
     # turn off namepsaces
@@ -95,3 +121,12 @@ if (__name__ == "__main__"):
     parser.setContentHandler(Handler)
 
     parser.parse("/Volumes/MUSIC/iTunes/iTunes Library.xml")
+
+    print()
+    print("Total iTunes entries parsed:  {}".format(total_itunes_entries))
+    print("Total candidate files to fix: {}".format(total_seen_files))
+    print("Total files actually fixed:   {}".format(total_updated_files))
+    if len(didnt_process) > 0:
+        print("Didn't process the following files:")
+        for i in didnt_process:
+            print("\t{}".format(i))
